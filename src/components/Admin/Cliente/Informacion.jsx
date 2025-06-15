37796dd9e82aecae9fbe2aaede3b04ca577e8e28
KarenHerreraDev <karenherrera.dev@gmail.com>
2025-06-15 18:44:35 -0300

    Se avanza con el footer

--- src/components/Admin/Cliente/Informacion.jsx
+++ src/components/Admin/Cliente/Informacion.jsx
@@ -1,93 +1,100 @@
 import React from "react";
-
+import useUserStore from "../../store/clienteStore"; 
 const Informacion = () => {
+  const { usuario } = useUserStore();
+
   return (
-    <div>
-      <div className="container mt-5">
-        <div className="row justify-content-center">
-          <div className="col-md-6">
-            <form className="formulario text-center ">
-              <div className="mb-3 formint">
-                <label htmlFor="nombre" className="form-label label-80">
-                  Nombre
-                </label>
-                <input
-                  type="text"
-                  className="form-control w-75 mx-auto inputform"
-                  id="nombre"
-                  placeholder="Nombre"
-                  name="nombre"
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <form className="formulario text-center">
+            <div className="mb-3 formint">
+              <label htmlFor="nombre" className="form-label label-80">
+                Nombre
+              </label>
+              <input
+                type="text"
+                className="form-control w-75 mx-auto inputform"
+                id="nombre"
+                name="nombre"
+                value={usuario.nombre}
+                disabled
+              />
+            </div>
 
-                />
-              </div>
+            <div className="mb-3">
+              <label htmlFor="apellido" className="form-label">
+                Apellido
+              </label>
+              <input
+                type="text"
+                className="form-control w-75 mx-auto inputform"
+                id="apellido"
+                value={usuario.apellido}
+                disabled
+              />
+            </div>
 
-              <div className="mb-3">
-                <label htmlFor="apellido" className="form-label">
-                  Apellido
-                </label>
-                <input
-                  type="text"
-                  className="form-control w-75 mx-auto inputform"
-                  id="apellido"
-                  placeholder="Apellido"
-                />
-              </div>
+            <div className="mb-3">
+              <label htmlFor="fecha" className="form-label">
+                Fecha de Nacimiento
+              </label>
+              <input
+                type="date"
+                className="form-control w-75 mx-auto inputform"
+                id="fecha"
+                value={usuario.fechaNac}
+                disabled
+              />
+            </div>
 
-              <div className="mb-3">
-                <label htmlFor="fecha" className="form-label">
-                  Fecha de Nacimiento
-                </label>
-                <input
-                  type="date"
-                  className="form-control w-75 mx-auto inputform"
-                  id="fecha"
-                  name="fechaNac"
-                />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="direccion" className="form-label">
-                  Dirección
-                </label>
-                <input
-                  type="text"
-                  className="form-control w-75 mx-auto inputform"
-                  id="direccion"
-                  placeholder="nombrecalle 123"
-                  name="direccion"
-                />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="correo" className="form-label">
-                  Correo Electrónico
-                </label>
-                <input
-                  type="text"
-                  className="form-control w-75 mx-auto inputform"
-                  id="correo"
-                  placeholder="name@example.com"
-                  name="correo"
-                />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="password" className="form-label">
-                  Contraseña
-                </label>
-                <input
-                  type="password"
-                  className="form-control w-75 mx-auto inputform"
-                  id="text"
-                  placeholder="abc123*"
-                  name="contraseña"
-                />
-              </div>
-              <button type="submit" className="btn btn-primary w-auto">
-                Cerrar
-              </button>
-            </form>
-          </div>
+            <div className="mb-3">
+              <label htmlFor="direccion" className="form-label">
+                Dirección
+              </label>
+              <input
+                type="text"
+                className="form-control w-75 mx-auto inputform"
+                id="direccion"
+                value={usuario.direccion}
+                disabled
+              />
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="correo" className="form-label">
+                Correo Electrónico
+              </label>
+              <input
+                type="text"
+                className="form-control w-75 mx-auto inputform"
+                id="correo"
+                value={usuario.correo}
+                disabled
+              />
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Contraseña
+              </label>
+              <input
+                type="password"
+                className="form-control w-75 mx-auto inputform"
+                id="password"
+                value={usuario.contraseña}
+                disabled
+              />
+            </div>
+
+            <button type="button" className="btn btn-primary w-auto">
+              Cerrar
+            </button>
+          </form>
         </div>
       </div>
     </div>
   );
 };
+
 export default Informacion;
--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -3,11 +3,63 @@ import { FaInstagramSquare, FaFacebook, FaWhatsapp } from "react-icons/fa";
 import "../../css/Footer.css";
 
 const Footer = () => {
+    const sucursales = [
+        {
+            nombre:"Yerba Buena",
+            ubicacion: "https://www.google.com/maps?q=26.8167,-65.3167",
+        },
+        {
+            nombre:"San Miguel de Tucumán",
+            ubicacion: "https://www.google.com/maps?q=26.8333,-65.2000",
+        },
+        {
+            nombre:"Alderetes",
+            ubicacion: "Ruta 307 Km 45",
+        }, 
+        {
+            nombre:"Concepción",
+            ubicacion: "https://www.google.com/maps?q=-26.9000,-65.1833",
+        }
+    ]
+    const servicios = [
+        {
+            icon: "bi bi-box-seam",
+            text: "Envios a Domicilio",
+        },
+        {
+            icon: "bi bi-credit-card",
+            text: "Pago con Tarjeta",
+        },
+        {
+            icon: "bi bi-telephone-fill",
+            text: "Asesoramiento personalizado",
+        }
+    ];
+
   return (
+    <>
+    <div className="servicios">
+        { servicios.map((servicio, index) => (
+          <div className="servicio-item" key={index}>
+            <i className={servicio.icon}></i>
+            <p>{servicio.text}</p>
+          </div>
+        )) }
+        
+      
+    </div>
     <div className="footer-container">
       <i className="bi bi-geo-alt-fill"></i>
-      <p>Rivadavia 1041 - San Miguel de Tucuman</p>
-      <footer className="footer-text">
+      <div className="footer">
+{                sucursales.map((sucursal, index) => (
+          <div key={index} className="sucursal">
+            <a className="no-link-style" href={sucursal.ubicacion}><h5>{sucursal.nombre}</h5></a>
+            
+          </div>
+        ))
+      }
+      </div>
+      <footer className="footer">
       <a
         href="https://www.instagram.com/karenherrera.bd?igsh=MW9ncGgxNWRqeDhoNg=="
         target="_blank"
@@ -37,6 +89,7 @@ const Footer = () => {
       </a>
       </footer>
     </div>
+    </>
   );
 };
 
