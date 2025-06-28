14852d19e14059d05931d516cdaaa114677f3c68
Matias <elmatiastomas@gmail.com>
2025-06-28 19:50:33 -0300

    se modifico el formulario de registro añadiendo validaciones funcionales

--- src/components/register/FormRegister.jsx
+++ src/components/register/FormRegister.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../../css/Register/MainRegister.css";
 import useCustomCliente from "../../CustomHooks/CustomCliente/useCustomCliente";
 import { toast } from "sonner";
+import { useNavigate } from "react-router-dom";
 
 const FormRegister = () => {
-  const {crearCliente}= useCustomCliente();
+  const navigate = useNavigate();
+  const {crearCliente, obtenerClientes }= useCustomCliente();
+  const [clientes, setClientes] = useState([]);
   const [nuevoCliente, setNuevoCliente] = useState({
     nombreCliente: "",
     apellidoCliente: "",
@@ -14,14 +17,68 @@ const FormRegister = () => {
     domicilioCliente: "",
     contraseña: "",
   });
+
+  useEffect(() => {
+    const traerClientes = async () => {
+      const data = await obtenerClientes();
+      setClientes(data || []);
+    };
+    traerClientes();
+  },[]);
+
   const ObtenerDatosDelCliente = (e)=> {
     e.preventDefault();
     setNuevoCliente({ ...nuevoCliente, [e.target.name]: e.target.value });
-
-  }
+   }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    
+    // Validar que todos los campos estén completos
+    if (
+      !nuevoCliente.nombreCliente.trim()||
+      !nuevoCliente.apellidoCliente.trim() ||
+      !nuevoCliente.DNI.trim() ||
+      !nuevoCliente.telefonoCliente.trim() ||
+      !nuevoCliente.emailCliente.trim() ||
+      !nuevoCliente.domicilioCliente.trim() ||
+      !nuevoCliente.contraseña.trim()
+    ) {
+      toast.error("Por favor, completa todos los campos");
+      return;
+    }
+    // Validar que DNI y teléfono sean solo números y sin espacios
+    if (!/^\d+$/.test(nuevoCliente.DNI.trim())) {
+      toast.error("El DNI debe contener solo números y sin espacios.");
+      return;
+    }
+    if (!/^\d+$/.test(nuevoCliente.telefonoCliente.trim())) {
+      toast.error("El teléfono debe contener solo números y sin espacios.");
+      return;
+    }
+    // Validar que el email tenga un formato correcto
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(nuevoCliente.emailCliente.trim())) { 
+      toast.error("Por favor, ingresa un correo electrónico válido.");
+      return;
+    }
+    // validar que los datos ingresados de email deni y telefono sean unicos
+    const existeDNI = clientes.some((cliente) => cliente.DNI === nuevoCliente.DNI.trim());
+    if (existeDNI) {
+      toast.error("El DNI ingresado ya está registrado.");
+      return;
+    }
+    const existeTelefono = clientes.some((cliente) => cliente.telefonoCliente === nuevoCliente.telefonoCliente.trim());
+    if (existeTelefono) { 
+      toast.error("El teléfono ingresado ya está registrado.");
+      return;
+    }
+    const existeEmail = clientes.some((cliente) => cliente.emailCliente === nuevoCliente.emailCliente.trim());
+    if (existeEmail) {
+      toast.error("El correo electrónico ingresado ya está registrado.");
+      return;
+    }
+    // Si todas las validaciones pasan, crear el cliente
     await crearCliente(nuevoCliente);
     toast.success("Cliente registrado correctamente");
     setNuevoCliente({
@@ -32,7 +89,12 @@ const FormRegister = () => {
       emailCliente: "",
       domicilioCliente: "",
       contraseña: "",
-  })}
+  });
+  //actualizar la lista de clientes
+  const data = await obtenerClientes()
+  setClientes(data || []);  
+  navigate("/login");
+};
 
   return (
     <div className="container mt-5">
